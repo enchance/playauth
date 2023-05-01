6801c5c7a5a372531100fe46ec222162582460cc
enchance <enchance@gmail.com>
2023-05-01 16:30:16 +0800

    update user using PUT

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 
 from app import settings as s, register_db, Env
@@ -29,4 +30,11 @@ app = get_app()
 @app.get('/')
 async def create_user():
     account = await Account.create(username='abc', email='abc@gmail.com', hashed_password='foobar')
+    return account
+
+@app.put('/')
+async def update(email: Annotated[str, Body()], username: Annotated[str, Body()]):
+    account = await Account.get(email=email).only('id', 'email', 'username')
+    account.username = username
+    await account.save(update_fields=['username'])
     return account
