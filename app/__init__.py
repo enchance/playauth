9059c941bf9d6878e381a211453e882b5e72e860
enchance <enchance@gmail.com>
2023-05-10 22:24:48 +0800

    create logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging, pytz
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+from fastapi.logger import logger
 from icecream import IceCreamDebugger
 
 from .schemas import *
@@ -6,5 +10,24 @@ from .db import register_db, red
 
 
 
+# Icecream
 ic = IceCreamDebugger(includeContext=True)
 ic.enabled = settings.DEBUG
+
+
+# Logs
+now = datetime.now(pytz.timezone('UTC'))
+formatter = '[%(asctime)s] %(levelname)s %(funcName)s:%(lineno)d - %(message)s'
+formatter = logging.Formatter(formatter)
+
+warning_handler = RotatingFileHandler(f'logs/{now:%Y-%m} warning.log',
+                                      maxBytes=500000, backupCount=10)
+warning_handler.setFormatter(formatter)
+warning_handler.setLevel(logging.WARNING)
+logger.addHandler(warning_handler)
+
+critical_handler = RotatingFileHandler(f'logs/{now:%Y-%m} critical.log',
+                                       maxBytes=500000, backupCount=10)
+critical_handler.setFormatter(formatter)
+critical_handler.setLevel(logging.CRITICAL)
+logger.addHandler(critical_handler)
